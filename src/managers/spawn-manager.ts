f5703aa18bc1f26f022b3d386158d9c92e1748f9
unmarco <un.marco@gmail.com>
2019-10-27 20:46:52 +0100

    cleanup/bugfix

--- src/managers/spawn-manager.ts
+++ src/managers/spawn-manager.ts
@@ -97,25 +97,6 @@ export class SpawnManager implements Manager {
                     spawner(tier, role.name);
                 }
             });
-
-            // Visuals
-            const v = new RoomVisual(room.name);
-
-            const numHarvesters = getByRole(RoleName.HARVESTER).length;
-            const maxHarvesters = (room.memory as RoomMemory).limits[RoleName.HARVESTER];
-            v.text(`⚡ H: ${numHarvesters}/${maxHarvesters}`, 17, 18, { align: 'left' });
-
-            const numUpgraders = getByRole(RoleName.UPGRADER).length;
-            const maxUpgraders = (room.memory as RoomMemory).limits[RoleName.UPGRADER];
-            v.text(`🔼 U: ${numUpgraders}/${maxUpgraders}`, 17, 19, { align: 'left' });
-
-            const numBuilders = getByRole(RoleName.BUILDER).length;
-            const maxBuilders = (room.memory as RoomMemory).limits[RoleName.BUILDER];
-            v.text(`🔨 B: ${numBuilders}/${maxBuilders}`, 17, 20, { align: 'left' });
-
-            const numRepairers = getByRole(RoleName.REPAIRER).length;
-            const maxRepairers = (room.memory as RoomMemory).limits[RoleName.REPAIRER];
-            v.text(`🔧 R: ${numRepairers}/${maxRepairers}`, 17, 21, { align: 'left' });
         }
     }
 }
